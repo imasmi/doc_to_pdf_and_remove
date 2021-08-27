32f1bdfacf14cdc74c559be4e2fa2184b3e386cc
imasmi.com <info@imasmi.com>
2021-08-27 12:22:14 +0300

    NAP 191 is fully ready

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
+use std::io;
 use std::fs;
+use std::string;
 use std::process::Command;
 
-
-fn main() {
-    Command::new("cmd")
-            .args(&["/C", "D:/Rust/Nap_convert/Doc2Pdf/bulk-convert-Word2PDF.bat"])
-            .output()
-            .expect("failed to execute process");
-
+fn convert_191(){
     for element in std::path::Path::new(r"C:/Users/1/Downloads/NAP_191").read_dir().unwrap() {
         let path = element.unwrap().path();
         if let Some(extension) = path.extension() {
@@ -15,5 +11,27 @@ fn main() {
                 fs::remove_file(path);
             }
         }
+    }
+}
+
+fn main() {
+    let mut x = String::new();
+    println! ("Избери НАП");
+    println! ("191: 1");
+    println! ("74: 2");
+    io::stdin().read_line(&mut x).expect("Failed to read line");
+    let y: u8 = x.trim().parse().expect("Не е номер");
+    if y == 1 {
+        Command::new("cmd")
+                .args(&["/C", "D:/Rust/Nap_convert/Doc2Pdf/bulk-convert-Word2PDF_NAP_191.bat"])
+                .output()
+                .expect("failed to execute process");
+        convert_191();
+    } else if y == 2{
+        Command::new("cmd")
+                .args(&["/C", "D:/Rust/Nap_convert/Doc2Pdf/bulk-convert-Word2PDF_NAP_74.bat"])
+                .output()
+                .expect("failed to execute process");
+
     }
 }
